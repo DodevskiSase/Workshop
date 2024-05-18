eb73ca80916ad87ba1d6e2a9f1b78329f229ad4e
Sase <sase.dod@gmail.com>
2024-05-18 18:21:42 +0200

    Finished ex 2

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let dayofWeek = "Monday";
 
-
-
+if (dayofWeek === "Monday") {
+  console.log("Start of the work week!");
+} else if (dayofWeek === "Wednesday") {
+  console.log("Midweek");
+} else if (dayofWeek === "Friday") {
+  console.log("Almost weekend!");
+} else {
+  console.log("Just another day");
+}
