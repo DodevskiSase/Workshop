36b83e6e56dccc86bf499b319406a5b1d89a899b
Sase <sase.dod@gmail.com>
2024-05-18 18:19:31 +0200

    finished ex 1

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let ticketPrices = 15;
 
-
-
+if (ticketPrices < 12) {
+  console.log("Child ticket: $5");
+} else if (ticketPrices < 18) {
+  console.log("Teen ticket: $7");
+} else if (ticketPrices > 18 < 60) {
+  console.log("Senior ticket: $10");
+} else {
+  console.log("Adult ticket: $8");
+}
